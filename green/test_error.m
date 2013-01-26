36e71b7738f521f36b1947c99d9730717458e0a7
ryz-mit <ryz@mit.edu>
2013-01-26 16:51:57 -0500

    Quadrature hankel transform + fixes quadht.m: - Implemented a Clenshaw-Curtis quadrature version of the hankel transform. Nodes and weights are generated using a fft method, allowing millions of points to be simultaneously quadratures. - The quadrature also gives an error estimate.
    test_error.m:
    - tests the error of the fdm against the analytical
    solution, gives a relative error estimate.
    
    Fixes:
    - analy_green: moved the 1e-7 factor to the end of
    the method.
    - fdm_run: moved the 1e-7 factor after the fdm.
    
    To do:
    - Implement new version of the fdm

--- green/test_error.m
+++ green/test_error.m
@@ -1,40 +1,41 @@
-function [A, r, rel_err, abs_err] = analy_green(z, zp, L)
-%ANALY_GREEN Analytical solution for Green's function
-%   Calculates the analytical solution for the Green's function of:
-%     half-space (2 half-spaces and one boundary)
-%     sandwich (3 intermediate layers, 4 boundaries)
+function [rel_err, abs_err] = test_error(z, zp, L)
+%TEST_ERROR Tests the FDM error for the half-space and 5-plate
+%configurations against analytical solutions
 %
-%   To do:
-%     plate (2 half-spaces, one intermediate "plate" layer, 2 boundaries)
 
-persistent k1
-persistent r1
-persistent I1
-%% Prepare Kernel
-zh = abs(z+zp);
-if isempty(I1)
-    R = 0.1;
-    K = 3e4;
-    [~,k1,r1,I1]=fht([],R,K,0,4,6);
+if nargin ==0
+z = 1e-3;
+zp = 5e-3;
+L = defaultL(2);
 end
 
+%% Pick the right layer configuration
+% Calculate using the FDM
+fprintf('Running FDM...\n');
+[A1 r z_actual] = fdm_run(z,zp,L);
+z = z_actual;
+
+%% Prepare Kernel
 mu=1e-7*4*pi;
 w = L.w;
+
+% Pick the correct analytical expression for the Green's funciton of such a
+% layered geometry.
 switch L.layerN
     case 2 % HALFSPACE
-        fprintf('Half-space configuration detected.\n');
+        fprintf('Recalculate using Half-Space Analytical formula.\n');
         
+        zh = abs(z+zp);
         mu_r = max(L.mu_r);
         sig = max(L.sig);
 
         % Setup analytical Green's functions
         eta = @(k,w) sqrt(k.^2+1j*w*mu*mu_r*sig);
         phi = @(k,w) (mu_r*k-eta(k,w))./(mu_r*k+eta(k,w));
-        Gr_ker = @(k) 1 ./ k .* (phi(k,w).* exp(-k.*(zh)));
-        inv_trans(Gr_ker);
+        integ = @(k) 1 ./ k .* (phi(k,w).* exp(-k.*(zh)));
         
     case 5 % SANDWICH
-        fprintf('Sandwich configuration detected.\n');
+        fprintf('Recalculate using Sandwich Analytical formula.\n');
         
         % coil layer
         c = L.coil_layer;
@@ -72,31 +73,34 @@ switch L.layerN
         % A1 is at z=d, A2 is at z=0
         % mu, mu_r, d, w should all be included in FDM_data.mat
         integ = @(k) 1 ./ k .* (g(k,w) + f(k,w)); 
-        inv_trans(integ);
         
-    otherwise
+    otherwise % should never happen!
         error('Number of layers provided: %d. This is not supported.',L.layerN);
-       
 end
-    function inv_trans(fun)
-    % Inverse hankel transform "fun" and derive estimates of the error.
-        A = (ifht(fun(k1),k1,r1,I1)+endcor(fun,k1(1),r1,0));
-        r = r1;
+    
+%%
+% Inverse hankel transform using quadrature.
+K = 1e4; % Maximum quadrature frequency.
+tic
+[A2, err] = quadht(integ,K,r,0,5e4); % use an intense quadrature to evaluate
+fprintf('---QUADRATURE---\nQuadrature time:\t%g seconds\n',toc);
+fprintf('Quadrature rel err:\t%1.1e\n',err/norm(A2,inf));
+fprintf('Rel Err due to cutoff:\t%1.1e\n---\n',integ(K)/norm(A2,inf));
+A2 = A2*1e-7; % renormalize to mu/4pi
 
-        r2 = linspace(r1(1),r1(end),5); % pick 5 points to test.
-        [A2] = quadht(fun,k1(end)*1e1,r2,0,2e6); % use an intense quadrature to evaluate
-        A2est = interp1(r1,A,r2);
-        
-        abs_err = max(abs(A2est-A2));
-        rel_err = abs_err/max(abs(A2));
-        if nargout < 3
-            fprintf('rel_err: %1.1e\tabs_err: %1.1e\n\n',rel_err,abs_err);
-        end
-        
-        % graphical treatment
-        loglog(k1,abs(real(fun(k1))),k1,abs(imag(fun(k1))));
-        
-        A = A*1e-7;
-    end
-end
+% % The following plots the integ function
+% k1 = K*logspace(-4,1);
+% figure(1)
+% loglog(k1,abs(real(integ(k1))),k1,abs(imag(integ(k1))));
 
+%% graphical treatment
+figure(2)
+subplot(211)
+loglog(r,abs(real(A1)),r,abs(real(A2)),r,abs(real(A1)-real(A2)));
+subplot(212)
+loglog(r,abs(imag(A1)),r,abs(imag(A2)),r,abs(imag(A1)-imag(A2)));
+
+%% Error estimates
+abs_err = norm(A2-A1,inf);
+rel_err = abs_err/norm(A2,inf);
+fprintf('---FDM--\nerr: %1.1e\trel err: %1.1e\n\n',abs_err,rel_err);
